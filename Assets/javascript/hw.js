080d0e78aaa8b7ebdd0e1bdd48b32a3d888043da
Daniel Vreeland <reddrum217@gmail.com>
2016-12-29 12:04:25 -0500

    Js Arry for Loop

--- Assets/javascript/hw.js
+++ Assets/javascript/hw.js
@@ -3,3 +3,17 @@
 // alert(hi);
 
 // http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC   
+
+// The array of my food choices
+var foods = ["Cherry Pie", "Brownies", "Cupcakes", "Macaroons", "Cookies", "Fudge", "Hot Cocoa", "Ice Cream"];
+
+for (i = 0; i < foods.length; i++) {
+
+	var btn = $("<button>").attr('class', 'btn btn-success');
+	btn.append(foods[i]);
+
+	$("#giphs").append(btn);
+
+	// $("#giphs").append("<button class='btn btn-success'>" + foods[i] + "</button>");
+
+};
